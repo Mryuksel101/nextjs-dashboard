045c7587827a1256a79ff311237be483aa15567f
mryuksel101 <mryuksel5@gmail.com>
2025-07-31 14:13:02 +0300

    Implement authentication flow with error handling; add login page and update sign-out functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,9 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -121,3 +124,23 @@ export async function deleteInvoice(id: string) {
     }
     revalidatePath('/dashboard/invoices');
 }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
+
